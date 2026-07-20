28ebe07eada0b47ef675e9bf9552d5f0d65e9f74
Marton Csernai <34937334+csmarc@users.noreply.github.com>
2026-07-20 19:10:07 +0100

    build: commit dist-esm output so git-dependency installs include the ESM build

---
+++ dist-esm/index.js
@@ -0,0 +1,5 @@
+export * from "./errors/MempoolApiError";
+export * from "./mempool/MempoolApi";
+export { MempoolBitcoinBlock } from "./mempool/MempoolBitcoinBlock";
+export * from "./mempool/MempoolBitcoinRpc";
+export * from "./synchronizer/MempoolBtcRelaySynchronizer";
---
+++ dist-esm/mempool/MempoolBitcoinBlock.js
@@ -0,0 +1,78 @@
+/**
+ * Bitcoin block representation as fetched from mempool.space
+ *
+ * @category Bitcoin
+ */
+export class MempoolBitcoinBlock {
+    constructor(obj) {
+        this.id = obj.id;
+        this.height = obj.height;
+        this.version = obj.version;
+        this.timestamp = obj.timestamp;
+        this.tx_count = obj.tx_count;
+        this.size = obj.size;
+        this.weight = obj.weight;
+        this.merkle_root = obj.merkle_root;
+        this.previousblockhash = obj.previousblockhash;
+        this.mediantime = obj.mediantime;
+        this.nonce = obj.nonce;
+        this.bits = obj.bits;
+        this.difficulty = obj.difficulty;
+    }
+    /**
+     * @inheritDoc
+     */
+    getHeight() {
+        return this.height;
+    }
+    /**
+     * @inheritDoc
+     */
+    getHash() {
+        return this.id;
+    }
+    /**
+     * @inheritDoc
+     */
+    getMerkleRoot() {
+        return this.merkle_root;
+    }
+    /**
+     * @inheritDoc
+     */
+    getNbits() {
+        return this.bits;
+    }
+    /**
+     * @inheritDoc
+     */
+    getNonce() {
+        return this.nonce;
+    }
+    /**
+     * @inheritDoc
+     */
+    getPrevBlockhash() {
+        return this.previousblockhash;
+    }
+    /**
+     * @inheritDoc
+     */
+    getTimestamp() {
+        return this.timestamp;
+    }
+    /**
+     * @inheritDoc
+     */
+    getVersion() {
+        return this.version;
+    }
+    /**
+     * @inheritDoc
+     *
+     * Not supported with mempool.space backend!
+     */
+    getChainWork() {
+        throw new Error("Unsupported");
+    }
+}
